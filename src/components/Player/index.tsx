a7e15be9406c5ddfe4c02c1c40774f1e1e94eb5a
Higor Cerqueira dos Santos <higorcsantos18@gmail.com>
2021-05-04 14:38:27 -0300

    First Commit

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,41 @@
+import styles from "./styles.module.scss";
+
+export function Player(){
+    return(
+        <div className={styles.playerContainer}>
+            <header>
+                <img src="/HeadPhone.svg"/>
+                <strong>Tocando Agora</strong>
+            </header>
+            <div className={styles.emptyPlayer}>
+                <strong>Selecione um podcast para ouvir</strong>            
+            </div>
+            <footer className={styles.empty}>
+                <div className={styles.progress}>
+                    <span>00:00</span>
+                    <div className={styles.slider}>
+                        <div className={styles.emptySlider}/>
+                    </div>
+                    <span>00:00</span>
+                </div>
+                <div className={styles.buttons}>
+                    <button type='button'>
+                        <img src="/shuffle.svg" alt="Embaralhar"/>
+                    </button>
+                    <button type='button'>
+                        <img src="/play-previous.svg" alt="Tocar Anterior"/>
+                    </button>
+                    <button type='button'>
+                        <img src="/play.svg" alt="Apertar o play"/>
+                    </button>
+                    <button type='button'>
+                        <img src="/play-next.svg" alt="Tocar próximo"/>
+                    </button>
+                    <button type='button'>
+                        <img src="/repeat.svg" alt="Repetir"/>
+                    </button>
+                </div>
+            </footer>
+        </div>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Header } from '../components/Header'
+
+
+export default function Home() {
+  return (
+      <h1>Index</h1>
+  )
+}
